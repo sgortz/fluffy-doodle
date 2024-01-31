eddb882cd6b22e69848614ca8e7b5e44cd59f156
sgortz <sabrina.gortz@outlook.com>
2024-01-30 22:22:35 -0500

    creating an employee and invoking methods

--- fluffy-doodle/Program.cs
+++ fluffy-doodle/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using fluffy_doodle;
+
 Console.WriteLine("Hello, World!");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@me.com", new DateTime(1979, 1, 16), 25);
+
+// invoking methods on the new object
+bethany.DisplayEmployeeDetails();
+
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork(5);
+bethany.PerformWork();
+
+double receivedWageBethany = bethany.ReceiveWage(true);
+Console.WriteLine($"Wage paid (message from Program): {receivedWageBethany}");
