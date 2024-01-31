e151b285ef32e0688aa1083a0098456bc387ecff
sgortz <sabrina.gortz@outlook.com>
2024-01-30 19:57:29 -0500

    Adds employee class

---
+++ fluffy-doodle/Employee.cs
@@ -0,0 +1,46 @@
+﻿namespace fluffy_doodle
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthday;
+
+        public void PerformWork()
+        {
+            numberOfHoursWorked++;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+        }
+
+        //method overloading
+        public void PerformWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+        }
+
+        // using optional parameters
+        public double ReceiveWage(bool resetHours = true)
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+
+            Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work.");
+
+            if(resetHours ) { numberOfHoursWorked = 0; }
+
+            return wage;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail: \t\t{email}\nBirthday: \t{birthday.ToShortDateString()}\n");     
+        }
+
+    }
+}
